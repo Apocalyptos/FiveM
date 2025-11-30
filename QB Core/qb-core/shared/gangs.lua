943f884bc8f83d4fba1e1ec54156ad0af9cc4fe8
Apocalyptos <apocalyptos@live.ca>
2025-11-30 09:38:58 -0500

    update

---
+++ QB Core/qb-core/shared/gangs.lua
@@ -0,0 +1,84 @@
+QBShared = QBShared or {}
+QBShared.Gangs = {
+    none = { label = 'Aucun Gang', grades = { ['0'] = { name = 'Non affilié' } } },
+    lostmc = {
+        label = 'The Lost MC',
+        grades = {
+            ['0'] = { name = 'Prospect' },
+            ['1'] = { name = 'Full Patch' },
+            ['2'] = { name = 'Capitain de route' },
+            ['3'] = { name = 'Sergent d’armes' },
+            ['4'] = { name = 'Trésorier' },
+            ['5'] = { name = 'Vice-Président' },
+			['6'] = { name = 'Président', isboss = true },
+        },
+    },
+    ballas = {
+        label = 'Ballas',
+        grades = {
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Dealer' },
+            ['3'] = { name = 'Lieutenant' },
+            ['4'] = { name = 'Capo' },
+			['5'] = { name = 'OG', isboss = true },
+        },
+    },
+    vagos = {
+        label = 'Vagos',
+        grades = {
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Dealer' },
+            ['3'] = { name = 'Lieutenant' },
+            ['4'] = { name = 'Capo' },
+			['5'] = { name = 'OG', isboss = true },
+        },
+    },
+    marabunta_grande = {
+        label = 'Marabunta Grande',
+        grades = {
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Dealer' },
+            ['3'] = { name = 'Lieutenant' },
+            ['4'] = { name = 'Capo' },
+			['5'] = { name = 'OG', isboss = true },
+        },
+    },
+    cartel = {
+        label = 'Cartel',
+        grades = {
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Mule' },
+            ['2'] = { name = 'Guetteur' },
+            ['3'] = { name = 'Tueur à gage' },
+			['4'] = { name = 'Lieutenant' },
+			['5'] = { name = 'Chef de place' },
+			['6'] = { name = 'Parrain', isboss = true },
+        },
+    },
+    families = {
+        label = 'Families',
+        grades = {
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Dealer' },
+            ['3'] = { name = 'Lieutenant' },
+            ['4'] = { name = 'Capo' },
+			['5'] = { name = 'OG', isboss = true },
+        },
+    },
+    triads = {
+        label = 'Triads',
+        grades = {
+            ['0'] = { name = 'Recrue' },
+            ['1'] = { name = 'Soldat' },
+            ['2'] = { name = 'Bras Armé' },
+            ['3'] = { name = 'Maître des rites' },
+            ['4'] = { name = 'Conseiller' },			
+            ['5'] = { name = 'Maître adjoint' },
+			['6'] = { name = 'Chef suprême', isboss = true },
+        },
+    }
+}
--- QB Core/qb-taxijob/locales/fr.lua
+++ QB Core/qb-taxijob/locales/fr.lua
@@ -1,10 +1,10 @@
 local Translations = {
     error = {
         ['already_mission'] = 'Vous êtes déjà en mission',
-        ['not_in_taxi'] = 'Vous n\'êtes pas dans un taxi',
-        ['missing_meter'] = "Ce véhicule n'a pas de taximètre",
-        ['no_vehicle'] = 'Vous n\'êtes pas dans un véhicule',
-        ['not_active_meter'] = 'Le taximètre n\'est pas activé',
+        ['not_in_taxi'] = 'Vous n’êtes pas dans un taxi',
+        ['missing_meter'] = 'Ce véhicule n’a pas de taximètre',
+        ['no_vehicle'] = 'Vous n’êtes pas dans un véhicule',
+        ['not_active_meter'] = 'Le taximètre n’est pas activé',
     },
     info = {
         ['person_was_dropped_off'] = 'La personne a été déposée!',
